cf9b4324de23d003d77e552898841dfc707a3179
Ronald Matimba <136543323+itsRon2@users.noreply.github.com>
2024-06-13 06:39:37 +0200

    modified:   types.ts

--- types.ts
+++ types.ts
@@ -1,23 +1,3 @@
-export interface Transaction {
-  id: number;
-  category_id: number;
-  amount: number;
-  date: number;
-  description: string;
-  type: "Expense" | "Income";
-}
-
-export interface Category {
-  id: number;
-  name: string;
-  type: "Expense" | "Income";
-}
-
-export interface TransactionsByMonth {
-  totalExpenses: number;
-  totalIncome: number;
-}
-
 export interface Donor {
   id: number;
   name: string;
